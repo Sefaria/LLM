244a716364e9786756a3be5f0fedc12d7b201688
nsantacruz <noahssantacruz@gmail.com>
2023-08-01 14:29:27 +0300

    chore: update antrhopic library

--- util/anthropic.py
+++ util/anthropic.py
@@ -1,14 +1,15 @@
 import anthropic
 import os
 
-async def get_completion_anthropic(prompt, max_tokens_to_sample: int = 100):
-    c = anthropic.Client(os.environ["ANTHROPIC_API_KEY"])
-    resp = await c.acompletion(
-        prompt=prompt,
+
+def get_completion_anthropic(prompt, max_tokens_to_sample: int = 5000):
+    c = anthropic.Client(api_key=os.environ["ANTHROPIC_API_KEY"])
+    resp = c.completions.create(
+        prompt=f"{anthropic.HUMAN_PROMPT} {prompt} {anthropic.AI_PROMPT}",
         stop_sequences=[anthropic.HUMAN_PROMPT],
         model="claude-v1",
         max_tokens_to_sample=max_tokens_to_sample,
         temperature=0,
     )
-    print("STOP REASON", resp['stop_reason'])
-    return resp['completion']
+    print("STOP REASON", resp.stop_reason)
+    return resp.completion
