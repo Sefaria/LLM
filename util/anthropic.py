599cde72796f037a04545b6cff4118b3df35e99c
nsantacruz <noahssantacruz@gmail.com>
2023-08-08 13:34:11 +0300

    feat: use claude instant

--- util/anthropic.py
+++ util/anthropic.py
@@ -2,14 +2,16 @@ import anthropic
 import os
 
 
-def get_completion_anthropic(prompt, max_tokens_to_sample: int = 5000):
+def get_completion_anthropic(prompt, max_tokens_to_sample: int = 1000000, verbose=True):
     c = anthropic.Client(api_key=os.environ["ANTHROPIC_API_KEY"])
     resp = c.completions.create(
         prompt=f"{anthropic.HUMAN_PROMPT} {prompt} {anthropic.AI_PROMPT}",
         stop_sequences=[anthropic.HUMAN_PROMPT],
-        model="claude-v1",
+        model="claude-instant-1",
         max_tokens_to_sample=max_tokens_to_sample,
         temperature=0,
     )
-    print("STOP REASON", resp.stop_reason)
+    if verbose:
+        print("NUM INPUT TOKENS", c.count_tokens(prompt))
+        print("STOP REASON", resp.stop_reason)
     return resp.completion
