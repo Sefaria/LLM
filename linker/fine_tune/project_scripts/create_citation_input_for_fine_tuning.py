64b35d9162e341b258f532507466f1018af84b9a
nsantacruz <noahssantacruz@gmail.com>
2023-09-04 20:07:39 +0300

    refactor: simplify create_citation_input_for_fine_tuning.py

--- linker/fine_tune/project_scripts/create_citation_input_for_fine_tuning.py
+++ linker/fine_tune/project_scripts/create_citation_input_for_fine_tuning.py
@@ -4,16 +4,8 @@ from sefaria.utils.util import wrap_chars_with_overlaps
 from sklearn.model_selection import train_test_split
 import srsly
 from util.general import load_mongo_docs
-from linker.fine_tune.project_scripts.constants import GPT_PROMPT_END_INDICATOR, GPT_COMPLETION_END_INDICATOR
+from linker.fine_tune.project_scripts import constants
 
-
-SPAN_LABEL_TO_CHAR_WRAPPER = {
-    "Person": ["{{", "}}"],
-    "Group-of-People": ["{{", "}}"],
-    "Group": ["{{", "}}"],
-    "Name-of-God": ["{{", "}}"],
-    "Citation": ["{{", "}}"],
-}
 
 SPAN_LABEL_TO_CLASSICATION_TAG = {
     "Person": "Person",
@@ -24,13 +16,11 @@ SPAN_LABEL_TO_CLASSICATION_TAG = {
 }
 
 
-def _get_wrap_chars(label):
-    return SPAN_LABEL_TO_CHAR_WRAPPER.get(label, None)
-
-
-def _get_wrapped_citation(citation, label):
-    wrapper_start, wrapper_end = _get_wrap_chars(label)
-    return f"{wrapper_start}{citation}{wrapper_end}", len(wrapper_start), len(wrapper_end)
+def _get_wrapped_citation(citation, metadata):
+    wrapped = f"{constants.ENTITY_PRE_WRAPPER}{citation}{constants.ENTITY_POST_WRAPPER}"
+    len_pre = len(constants.ENTITY_PRE_WRAPPER)
+    len_post = len(constants.ENTITY_POST_WRAPPER)
+    return wrapped, len_pre, len_post
 
 
 def get_window_around_match(start_char:int, end_char:int, text:str, window:int=10) -> tuple:
@@ -55,21 +45,17 @@ class GptNerTrainingGenerator:
 
     @staticmethod
     def _create_prompt(doc):
-        return f"{doc['text']} {GPT_PROMPT_END_INDICATOR}"
+        return f"{doc['text']} {constants.GPT_PROMPT_END_INDICATOR}"
 
     @staticmethod
     def _create_completion(doc):
-        chars_to_wrap = [(span['start'], span['end'], span['label']) for span in doc['spans'] if span['label'] in
-                         SPAN_LABEL_TO_CHAR_WRAPPER]
+        chars_to_wrap = [(span['start'], span['end'], None) for span in doc['spans'] if span['label'] in
+                         SPAN_LABEL_TO_CLASSICATION_TAG]
         wrapped_chars = wrap_chars_with_overlaps(doc['text'], chars_to_wrap, _get_wrapped_citation)
-        return f" {wrapped_chars}{GPT_COMPLETION_END_INDICATOR}"
+        return f" {wrapped_chars}{constants.GPT_COMPLETION_END_INDICATOR}"
 
 
 class GptEntityClassificationTrainingGenerator:
-
-    def __init__(self, before_wrapper="{{", after_wrapper="}}"):
-        self.before_wrapper = before_wrapper
-        self.after_wrapper = after_wrapper
 
     def generate(self, docs):
         data = []
@@ -84,12 +70,12 @@ class GptEntityClassificationTrainingGenerator:
     def create_prompt(self, text, start, end):
         before_window, after_window = get_window_around_match(start, end, text)
         span_text = text[start:end]
-        wrapped_chars = f"{before_window} {self.before_wrapper}{span_text}{self.after_wrapper} {after_window}"
-        return f"{wrapped_chars} {GPT_PROMPT_END_INDICATOR}"
+        wrapped_chars = f"{before_window} {constants.ENTITY_PRE_WRAPPER}{span_text}{constants.ENTITY_POST_WRAPPER} {after_window}"
+        return f"{wrapped_chars} {constants.GPT_PROMPT_END_INDICATOR}"
 
     @staticmethod
     def _create_completion(doc, span):
-        return f" {SPAN_LABEL_TO_CLASSICATION_TAG[span['label']]} {GPT_COMPLETION_END_INDICATOR}"
+        return f" {SPAN_LABEL_TO_CLASSICATION_TAG[span['label']]} {constants.GPT_COMPLETION_END_INDICATOR}"
 
 
 def get_gpt_training_data(task, docs):
