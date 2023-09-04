7bb51fabdbd4aa61a7882c6353a78f85fac57dd6
nsantacruz <noahssantacruz@gmail.com>
2023-09-04 13:07:29 +0300

    feat: add sampling code to sentencize_web_input.py

--- linker/fine_tune/one_time_scripts/sentencize_web_input.py
+++ linker/fine_tune/one_time_scripts/sentencize_web_input.py
@@ -6,7 +6,7 @@ from pymongo import InsertOne
 from tqdm import tqdm
 
 
-if __name__ == '__main__':
+def sentencize_web_input():
     my_db = MongoProdigyDBManager("ner_en_sent_web_input")
     my_db.output_collection.delete_many({})
     docs = list(load_mongo_docs(collection))
@@ -18,5 +18,16 @@ if __name__ == '__main__':
             doc_copy = doc.copy()
             doc_copy['text'] = sent
             sent_docs += [doc_copy]
-    my_db.output_collection.bulk_write(InsertOne(d) for d in sent_docs)
+    my_db.output_collection.bulk_write([InsertOne(d) for d in sent_docs])
+
+def output_sample():
+    docs = load_mongo_docs("ner_en_sent_web_input")
+    for doc in docs:
+        print(doc['text'])
+        print()
+
+
+if __name__ == '__main__':
+    # sentencize_web_input()
+    output_sample()
 
