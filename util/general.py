454905cc8104e4debfe1676c945da9fa19e2cd3f
nsantacruz <noahssantacruz@gmail.com>
2023-09-04 13:05:30 +0300

    feat: add get_removal_list function

--- util/general.py
+++ util/general.py
@@ -3,6 +3,7 @@ django.setup()
 from db_manager import MongoProdigyDBManager
 from sefaria.model.text import Ref
 from sefaria.helper.normalization import NormalizerComposer
+import diff_match_patch
 
 
 def get_normalizer():
@@ -28,3 +29,28 @@ def get_ref_text_with_fallback(oref: Ref, lang: str) -> str:
         raw_text = get_raw_ref_text(oref, other_lang)
 
     return normalizer.normalize(raw_text)
+
+
+def get_removal_list(orig, new):
+    """
+    Calculate list of text removed (or added) in order to convert `orig` into `new`
+    :param orig: original string
+    :param new: new string. assumption is it is quite similar to `orig`
+    :return: list where each element is ((start_char, end_char), text_added).
+    If text was deleted, `text_added` will be '' and the range will cover the range of text deleted.
+    If text was added, the range will be zero-length
+    """
+    checker = diff_match_patch.diff_match_patch()
+    diff = checker.diff_main(orig, new)
+    removal_list = []
+    curr_start_char = 0
+    for diff_type, diff_text in diff:
+        if diff_type == 0:
+            curr_start_char += len(diff_text)
+        elif diff_type == 1:
+            removal_list += [((curr_start_char, curr_start_char), diff_text)]
+            curr_start_char += len(diff_text)
+        elif diff_type == -1:
+            removal_list += [((curr_start_char, curr_start_char + len(diff_text)), '')]
+    removal_list.sort(key=lambda x: (x[0][0], (x[0][1]-x[0][0])))
+    return removal_list
