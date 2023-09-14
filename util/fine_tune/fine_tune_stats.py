dd992563deaa9cfe7e0605f4100e62e5fc67ddea
yonadavGit <yonadav.le@gmail.com>
2023-09-14 13:21:13 +0300

    chore: neatly getting content of results file using File object, getting api key from os not as parameter

--- util/fine_tune/fine_tune_stats.py
+++ util/fine_tune/fine_tune_stats.py
@@ -1,29 +1,14 @@
 import typer
 import subprocess
-from openai import File
+import openai
+import os
+# from openai import File
 
-def fine_tune(openai_api_key: str, results_file_id: str, output_file: str):
-    # Check if required inputs are provided
-    if not (openai_api_key and results_file_id and output_file):
-        typer.echo("Error: Missing required parameters")
-        typer.echo("Usage: ./fine_tune.py fine-tune <OPENAI_API_KEY> <RESULTS_FILE_ID> <OUTPUT_FILE>")
-        raise typer.Exit(code=1)
-
-    headers = {
-        "Authorization": f"Bearer {openai_api_key}"
-    }
-
-    # Get via File object
-    output_file_object = File.download(results_file_id, openai_api_key)
-
-    # Get via File curl command
-    curl_command = [
-        "curl",
-        f"https://api.openai.com/v1/files/{results_file_id}/content",
-        "-H", f"-H Authorization: Bearer {openai_api_key}",
-        "-o", output_file
-    ]
-    subprocess.run(curl_command, check=True, text=True)
+def fine_tune(results_file_id: str, output_file: str):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    content = openai.File.download(results_file_id)
+    with open(output_file, 'wb') as file:
+        file.write(content)
 
 if __name__ == "__main__":
     typer.run(fine_tune)
