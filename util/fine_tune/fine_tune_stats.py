b697085dfaecf7efa79d1010a56c8593e9d19f8d
yonadavGit <yonadav.le@gmail.com>
2023-09-14 13:57:04 +0300

    chore: get results file id from last job, get rid of this parameter in .yml

--- util/fine_tune/fine_tune_stats.py
+++ util/fine_tune/fine_tune_stats.py
@@ -1,11 +1,13 @@
 import typer
-import subprocess
 import openai
 import os
 # from openai import File
 
-def fine_tune(results_file_id: str, output_file: str):
+def fine_tune(output_file: str):
     openai.api_key = os.getenv("OPENAI_API_KEY")
+    fine_tune_jobs = openai.FineTuningJob.list()['data']
+    last_job = max(fine_tune_jobs, key=lambda x: x.created_at)
+    results_file_id = last_job['result_files'][0]
     content = openai.File.download(results_file_id)
     with open(output_file, 'wb') as file:
         file.write(content)
