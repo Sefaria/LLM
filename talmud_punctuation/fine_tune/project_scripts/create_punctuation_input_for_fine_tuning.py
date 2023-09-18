6d5c33ead3f7c3f937c6fd6f2ea3dfeab546cc26
yonadavGit <yonadav.le@gmail.com>
2023-09-18 13:16:43 +0300

    chore: sampling traning and valdition data using a pre-determined seed

--- talmud_punctuation/fine_tune/project_scripts/create_punctuation_input_for_fine_tuning.py
+++ talmud_punctuation/fine_tune/project_scripts/create_punctuation_input_for_fine_tuning.py
@@ -7,6 +7,11 @@ from sefaria.model import *
 from sefaria.utils.hebrew import strip_cantillation
 import random
 
+seed_value = 613
+random.seed(seed_value)
+sample_size = 10
+
+train_proportion = 0.8
 
 masechtot_ordered = ["Berakhot", "Shabbat", "Eruvin", "Pesachim", "Rosh Hashanah", "Yoma", "Sukkah", "Beitzah",
                           "Taanit", "Megillah", "Moed Katan", "Chagigah",
@@ -16,13 +21,10 @@ masechtot_ordered = ["Berakhot", "Shabbat", "Eruvin", "Pesachim", "Rosh Hashanah
                           "Zevachim", "Menachot", "Chullin", "Bekhorot", "Arakhin", "Temurah", "Keritot", "Meilah",
                           "Tamid", "Niddah"]
 
-training_examples = []
-validation_examples = []
 task_desciption = "Given a pair ('no_punctuation_talmud': <text>, 'steinsaltz': <text>), where 'no_punctuation_talmud' is a passage from the Talmud without punctuation marks and 'steinsaltz' is a passage that contains the Talmudic text, expands on it, and interprets it, output a punctuated version of the Talmudic passage using the punctuation that appears in the 'steinsaltz' interpretation."
-last_masechet = "Gittin"
+last_masechet = "Berakhot"
 
-def true_with_probability(p):
-    return random.random() < p
+
 def create_new_context(task_desciption, non_punctuated, steinsalz, punctuated):
     return (
     {"messages": [{"role": "system", "content": task_desciption},
@@ -31,6 +33,7 @@ def create_new_context(task_desciption, non_punctuated, steinsalz, punctuated):
     )
 
 def create_data(output_training_filename: str, output_validation_filename: str):
+    all_samples = []
     for masechet in masechtot_ordered:
         print("creating data from Masechet " + masechet)
         all_segment_refs = Ref(masechet).all_segment_refs()
@@ -38,17 +41,26 @@ def create_data(output_training_filename: str, output_validation_filename: str):
             non_punctuated = segment_ref.text('he', "William Davidson Edition - Aramaic").text
             punctuated = strip_cantillation(segment_ref.text('he').text, strip_vowels=True)
             steinsalz = Ref("Steinsaltz on " + segment_ref.normal()).text('he').text
-            if true_with_probability(0.8):
-                training_examples.append(create_new_context(task_desciption, non_punctuated, steinsalz, punctuated))
-            else:
-                validation_examples.append(create_new_context(task_desciption, non_punctuated, steinsalz, punctuated))
+            all_samples.append(create_new_context(task_desciption, non_punctuated, steinsalz, punctuated))
         if masechet == last_masechet:
             break
 
-    srsly.write_jsonl(output_training_filename, training_examples)
-    srsly.write_jsonl(output_validation_filename, validation_examples)
-    print("TRAINING SAMPLES: "  + str(len(training_examples)))
-    print("VALIDATION SAMPLES: " + str(len(validation_examples)))
+    #get only limited num of samples
+    samples_trimmed = []
+    samples_trimmed = random.sample(all_samples, sample_size)
+
+    # Calculate the number of items for training
+    num_train = int(len(samples_trimmed) * train_proportion)
+
+    # Use random.sample to partition the list according to the seed
+    train_samples = random.sample(samples_trimmed, num_train)
+    validation_samples = [item for item in samples_trimmed if item not in train_samples]
+
+    srsly.write_jsonl(output_training_filename, train_samples)
+    srsly.write_jsonl(output_validation_filename, validation_samples)
+
+    print("TRAINING SAMPLES: "  + str(len(train_samples)))
+    print("VALIDATION SAMPLES: " + str(len(validation_samples)))
 
 if __name__ == '__main__':
     typer.run(create_data)
