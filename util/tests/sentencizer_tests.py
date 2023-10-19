cc0e470b3cecdd474356b8c1896536a6b6e7a544
nsantacruz <noahssantacruz@gmail.com>
2023-10-19 10:36:31 +0300

    feat: improve sentencizer to avoid short sentences.

---
+++ util/tests/sentencizer_tests.py
@@ -0,0 +1,14 @@
+import pytest
+from util.sentencizer import _combine_small_sentences
+
+
+@pytest.mark.parametrize(('sentences', 'expected_output'), [
+    [['a b', 'c d'], ['a b', 'c d']],
+    [['a', 'c d'], ['a c d']],
+    [['a', 'c d', 'e'], ['a c d e']],
+    [['a b', 'c', 'e'], ['a b c e']],
+    [['a b', 'c', 'e f', 'g h'], ['a b c e f', 'g h']],
+])
+def test_combine_small_sentences(sentences, expected_output):
+    actual_output = _combine_small_sentences(sentences, min_words=2)
+    assert actual_output == expected_output
