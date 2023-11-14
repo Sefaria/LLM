5b908c21fd5c091b94fe40ddef052790de490b1a
yonadavGit <yonadav.le@gmail.com>
2023-11-14 14:59:43 +0200

    implemented clustering function for topic embeddings

--- topic_tagging/playground.py
+++ topic_tagging/playground.py
@@ -99,7 +99,69 @@ def embed_topic_descriptions_to_jsonl(slugs_and_descriptions_csv, output_jsonl_p
         for item in list_of_embeddings:
             jsonl_file.write(json.dumps(item) + '\n')
 
+def cluster_slugs():
+    import numpy as np
+    from sklearn.cluster import KMeans
+    from sklearn.metrics import pairwise_distances_argmin_min
+    import matplotlib.pyplot as plt
 
+    # Assuming you have a list of dicts with slugs and embeddings
+    data = [
+        {"slug": "slug1", "embedding": np.array([0.1, 0.2, 0.3])},
+        {"slug": "slug2", "embedding": np.array([0.4, 0.5, 0.6])},
+        # ... more data ...
+    ]
+
+    data = [json.loads(line) for line in open('description_embeddings.jsonl', 'r')]
+
+
+    # Extract embeddings into a NumPy array
+    embeddings = np.array([item["embedding"] for item in data])
+
+    # Choose the number of clusters (you might need to tune this)
+    num_clusters = 5
+
+    # Perform K-means clustering
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    kmeans.fit(embeddings)
+
+    # Get the cluster labels
+    cluster_labels = kmeans.labels_
+
+    # Assign each data point to its nearest cluster
+    closest_points, _ = pairwise_distances_argmin_min(embeddings, kmeans.cluster_centers_)
+
+    # Create a dictionary to store the clusters
+    clusters = {i: [] for i in range(num_clusters)}
+
+    # Assign each data point to its cluster in the dictionary
+    for i, point_index in enumerate(closest_points):
+        clusters[cluster_labels[i]].append(data[i]["slug"])
+
+    # Print the clusters
+    for cluster_label, cluster_slugs in clusters.items():
+        print(f"Cluster {cluster_label + 1}: {cluster_slugs}")
+
+    # Plot the clusters
+    plt.figure(figsize=(24, 16))
+
+    for i in range(num_clusters):
+        cluster_points = embeddings[cluster_labels == i]
+        plt.scatter(cluster_points[:, 0], cluster_points[:, 1], label=f'Cluster {i + 1}')
+
+    # Plot the cluster centers
+    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='x', s=200, c='red',
+                label='Centroids')
+
+    # Annotate points with slugs
+    for i, txt in enumerate([item["slug"] for item in data]):
+        plt.annotate(txt, (embeddings[i, 0], embeddings[i, 1]), textcoords="offset points", xytext=(0, 5), ha='center')
+
+    plt.title('K-means Clustering')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.legend()
+    plt.show()
 if __name__ == '__main__':
     print("Hi")
     # get_embeddings()
@@ -108,7 +170,8 @@ if __name__ == '__main__':
     # Description:
     # """
     # infer_topic_descriptions_to_csv("n_topic_slugs.csv", template, "slugs_and_inferred_descriptions.csv")
-    embed_topic_descriptions_to_jsonl("slugs_and_inferred_descriptions.csv", "description_embeddings.jsonl")
+    # embed_topic_descriptions_to_jsonl("slugs_and_inferred_descriptions.csv", "description_embeddings.jsonl")
+    cluster_slugs()
 
 
 
