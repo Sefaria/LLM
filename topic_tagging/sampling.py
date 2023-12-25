59e44c15f0c5505db9a85e8ebab7c754bc292036
yonadavGit <yonadav.le@gmail.com>
2023-12-25 13:17:47 +0200

    feat(ValidationSet): script creating sample of refs end to end

--- topic_tagging/sampling.py
+++ topic_tagging/sampling.py
@@ -1,6 +1,7 @@
 import django
 django.setup()
 from sefaria.model import *
+import csv
 import os
 import random
 from tqdm import tqdm
@@ -9,7 +10,7 @@ random.seed(613)
 def get_all_refs_from_category(topic_category):
     source_refs = []
     sub_topics = topic_category.topics_by_link_type_recursively(linkType='displays-under')
-    for sub_topic in sub_topics:
+    for sub_topic in tqdm(sub_topics, desc=f"Processing sub topics for category {topic_category}", unit="sub_topic"):
         sources = [*sub_topic.link_set(_class="refTopic").array()]
         sources = [source for source in sources if not source.ref.startswith("Sheet")]
         source_refs += [source.ref for source in sources]
@@ -36,6 +37,7 @@ def get_all_seg_refs_from_library_category(category: str):
 def create_quintessence_of_texts_by_library_noahs_method(sample_size = 5000) -> Dictionary:
     sample = {}
     top_level_categories = library.get_top_categories()
+    top_level_categories = [cat for cat in top_level_categories if cat != "Reference"]
     cat_sample_size = int(sample_size / len(top_level_categories))
     for category in top_level_categories:
         cat_refs = get_all_seg_refs_from_library_category(category)
@@ -69,6 +71,40 @@ def create_quintessence_of_texts_by_topics_noahs_method_to_csv():
     samples = create_quintessence_of_texts_by_topic_noahs_method()
     dict_of_lists_to_csv("quintessence_of_topics_noah_method.csv", samples)
 
+def read_refs_from_csv(path_to_csv):
+    refs = []
+    with open(path_to_csv, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)
+        for row in reader:
+            refs += row
+    return refs
+
+def mix_quintessences(topic_refs, library_refs, topic_refs_num=100, library_refs_num=75):
+    mix = []
+    mix += random.sample(topic_refs, topic_refs_num)
+    mix += random.sample(library_refs, library_refs_num)
+    return mix
+
+def create_sample_end_to_end():
+    topic_refs_dict = create_quintessence_of_texts_by_topic_noahs_method()
+    library_refs_dict = create_quintessence_of_texts_by_library_noahs_method()
+
+    topic_refs = [value for values in topic_refs_dict.values() for value in values]
+    library_refs = [value for values in library_refs_dict.values() for value in values]
+
+    sample = mix_quintessences(topic_refs, library_refs)
+
+    return sample
+
+def sample_end_to_end_to_csv(file_name="refs_sample.csv"):
+    sample = create_sample_end_to_end()
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        for item in sample:
+            writer.writerow([item])
+
 if __name__ == "__main__":
-    create_quintessence_of_texts_by_topics_noahs_method_to_csv()
+    sample_end_to_end_to_csv()
+
 
