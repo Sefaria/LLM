d4eed939ea04f207cd5f4e7fea20c7b7b28e9099
nsantacruz <noahssantacruz@gmail.com>
2024-02-01 14:13:53 +0200

    feat: add support for connecting to sentinel

--- celery_config.py
+++ celery_config.py
@@ -1,5 +1,32 @@
 import os
+import dns.resolver
 
-broker_url = os.getenv('CELERY_BROKER_URL')  # 'redis://127.0.0.1:6379/2'
-broker_transport_options = {}
-result_backend = os.getenv('CELERY_BACKEND_URL')  # 'redis://127.0.0.1:6379/3'
+redis_port = os.getenv('REDIS_PORT')
+broker_db_num = os.getenv('REDIS_BROKER_DB_NUM')
+result_backend_db_num = os.getenv('REDIS_RESULT_BACKEND_DB_NUM')
+
+# Either define SENTINEL_HEADLESS_URL if using sentinel or REDIS_URL for a simple redis instance
+sentinel_url = os.getenv('SENTINEL_HEADLESS_URL')
+redis_url = os.getenv('REDIS_URL')
+
+
+def add_db_num_to_url(url, db_num):
+    return url.replace(f':{redis_port}', f':{redis_port}/{db_num}')
+
+
+if sentinel_url:
+    redisdns = dns.resolver.resolve(sentinel_url, 'A')
+    addressstring = []
+    for res in redisdns.response.answer:
+        for item in res.items:
+            addressstring.append(f"sentinel://{item.to_text()}:{redis_port}")
+    joined_address = ";".join(addressstring)
+
+    # celery config vars
+    broker_url = add_db_num_to_url(joined_address, broker_db_num)
+    result_backend = add_db_num_to_url(joined_address, result_backend_db_num)
+    result_backend_transport_options = {}
+    broker_transport_options = {}
+else:
+    broker_url = add_db_num_to_url(f"{redis_url}:{redis_port}", broker_db_num)
+    result_backend = add_db_num_to_url(f"{redis_url}:{redis_port}", broker_db_num)
