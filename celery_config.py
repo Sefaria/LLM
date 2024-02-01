5d2eabf3e1cf71621248e15485d35f50ce4b36d0
nsantacruz <noahssantacruz@gmail.com>
2024-02-01 12:10:50 +0200

    feat: read celery config values from envvars

--- celery_config.py
+++ celery_config.py
@@ -1,3 +1,5 @@
-broker_url = 'redis://127.0.0.1:6379/2'
+import os
+
+broker_url = os.getenv('CELERY_BROKER_URL')  # 'redis://127.0.0.1:6379/2'
 broker_transport_options = {}
-result_backend = 'redis://127.0.0.1:6379/3'
+result_backend = os.getenv('CELERY_BACKEND_URL')  # 'redis://127.0.0.1:6379/3'
