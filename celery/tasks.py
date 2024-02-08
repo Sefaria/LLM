dd5ab7a4da1f216028250efe7c87746e9557374f
nsantacruz <noahssantacruz@gmail.com>
2024-02-08 09:39:07 +0200

    feat: change output of generate_topic_prompts

--- celery/tasks.py
+++ celery/tasks.py
@@ -8,10 +8,13 @@ app.config_from_object('celery.config')
 
 
 @app.task()
-def generate_topic_prompts(raw_topic_prompt_input: dict) -> List[dict]:
+def generate_topic_prompts(raw_topic_prompt_input: dict) -> dict:
     tp_input = TopicPromptInput.create(raw_topic_prompt_input)
     toprompt_options_list = get_toprompts(tp_input)
     # only return the first option for now
-    return [
-       options.toprompts[0].serialize() for options in toprompt_options_list
-    ]
+    return {
+        "lang": tp_input.lang,
+        "prompts": [
+           options.toprompts[0].serialize() for options in toprompt_options_list
+        ]
+    }
