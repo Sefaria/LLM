316da87a3d6228637f3849de8edf3553d23db10d
nsantacruz <noahssantacruz@gmail.com>
2024-03-05 12:54:11 +0200

    fix: add sentinel pw to transport options

--- app/celery_setup/config.py
+++ app/celery_setup/config.py
@@ -10,6 +10,7 @@ result_backend_db_num = os.getenv('REDIS_RESULT_BACKEND_DB_NUM')
 # Either define SENTINEL_HEADLESS_URL if using sentinel or REDIS_URL for a simple redis instance
 sentinel_url = os.getenv('SENTINEL_HEADLESS_URL')
 sentinel_transport_opts = json.loads(os.getenv('SENTINEL_TRANSPORT_OPTS', '{}'))
+sentinel_password = os.getenv('SENTINEL_PASSWORD')
 redis_url = os.getenv('REDIS_URL')
 redis_password = os.getenv('REDIS_PASSWORD')
 
@@ -36,7 +37,7 @@ if sentinel_url:
     broker_url = add_db_num_to_url(joined_address, broker_db_num)
     result_backend = add_db_num_to_url(joined_address, result_backend_db_num)
     result_backend_transport_options = sentinel_transport_opts
-    broker_transport_options = sentinel_transport_opts
+    broker_transport_options = {**sentinel_transport_opts, "sentinel_kwargs": {"password": sentinel_password}}
 else:
     redis_url = add_password_to_url(f"{redis_url}:{redis_port}", redis_password)
     broker_url = add_db_num_to_url(redis_url, broker_db_num)
