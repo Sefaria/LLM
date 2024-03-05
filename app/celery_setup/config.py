a4fd955821387e6bbffb8af19fb5f4c096776ec9
nsantacruz <noahssantacruz@gmail.com>
2024-03-05 10:59:47 +0200

    fix: use sentinel transport opts env var

--- app/celery_setup/config.py
+++ app/celery_setup/config.py
@@ -1,5 +1,6 @@
 import os
 import dns.resolver
+import json
 
 redis_port = os.getenv('REDIS_PORT')
 broker_db_num = os.getenv('REDIS_BROKER_DB_NUM')
@@ -7,6 +8,7 @@ result_backend_db_num = os.getenv('REDIS_RESULT_BACKEND_DB_NUM')
 
 # Either define SENTINEL_HEADLESS_URL if using sentinel or REDIS_URL for a simple redis instance
 sentinel_url = os.getenv('SENTINEL_HEADLESS_URL')
+sentinel_transport_opts = json.loads(os.getenv('SENTINEL_TRANSPORT_OPTS', '{}'))
 redis_url = os.getenv('REDIS_URL')
 
 
@@ -25,8 +27,8 @@ if sentinel_url:
     # celery config vars
     broker_url = add_db_num_to_url(joined_address, broker_db_num)
     result_backend = add_db_num_to_url(joined_address, result_backend_db_num)
-    result_backend_transport_options = {}
-    broker_transport_options = {}
+    result_backend_transport_options = sentinel_transport_opts
+    broker_transport_options = sentinel_transport_opts
 else:
     broker_url = add_db_num_to_url(f"{redis_url}:{redis_port}", broker_db_num)
     result_backend = add_db_num_to_url(f"{redis_url}:{redis_port}", result_backend_db_num)
