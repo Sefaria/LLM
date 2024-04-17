1d1f486b85b75d07aeb2994b4eecb88484539925
nsantacruz <noahssantacruz@gmail.com>
2024-04-17 16:09:13 +0300

    feat: add function that can check if a text is about a topic

--- experiments/topic_source_curation/common.py
+++ experiments/topic_source_curation/common.py
@@ -2,10 +2,11 @@ import json
 
 import numpy as np
 
+from util.general import get_by_xml_tag
 from sefaria_llm_interface.common.topic import Topic
 from sefaria_llm_interface.topic_prompt import TopicPromptSource
 from sefaria_llm_interface.topic_source_curation import CuratedTopic
-from basic_langchain.chat_models import ChatOpenAI
+from basic_langchain.chat_models import ChatOpenAI, ChatAnthropic
 from basic_langchain.schema import SystemMessage, HumanMessage
 from topic_prompt.uniqueness_of_source import summarize_based_on_uniqueness
 from basic_langchain.embeddings import OpenAIEmbeddings
@@ -30,3 +31,18 @@ def get_datasets():
 
 def get_exported_topic_pages():
     return _get_dataset(EXPORTED_TOPIC_PAGES)
+
+
+def is_text_about_topic(topic_description, text):
+    llm = ChatAnthropic(model='claude-3-haiku-20240229', temperature=0)
+    system = SystemMessage(content="You are a Jewish scholar. Given a topic description wrapped in <topic> and a text, "
+                                   "wrapped in <text>, output 'Yes' if <text> is about <topic> and 'No' if <text> is "
+                                   "not about <topic>. Wrap output in <answer> tags.")
+    human = HumanMessage(content=f"<topic>{topic_description}</topic>\n<text>{text}</text>")
+    response = llm([system, human])
+    answer = get_by_xml_tag(response.content, 'answer').strip()
+    if answer not in {'Yes', 'No'}:
+        print(f"Answer not in Yes or No: {answer}")
+        return False
+    return answer == 'Yes'
+
