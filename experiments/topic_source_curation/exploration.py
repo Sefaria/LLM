2abca7054caef1df300194b441a4c7f7d110c79d
nsantacruz <noahssantacruz@gmail.com>
2024-04-17 09:55:56 +0300

    chore: calculate which Hebrew books could use translation

--- experiments/topic_source_curation/exploration.py
+++ experiments/topic_source_curation/exploration.py
@@ -1,28 +1,47 @@
 """
 Functions to explore existing topic curations
 """
-from topic_source_curation.common import get_datasets
+from experiments.topic_source_curation.common import get_datasets
 from collections import defaultdict
 import django
 django.setup()
 from sefaria.model import *
+from tqdm import tqdm
 
 
 if __name__ == '__main__':
     bad, good = get_datasets()
-    counts = defaultdict(int)
-    for example in good:
+    counts = defaultdict(list)
+    for example in tqdm(good):
         for source in example.sources:
             oref = Ref(source.ref)
             index = oref.index
+            he_versions = VersionSet({"title": index.title, "actualLanguage": "he"}).array()
+            if len(he_versions) == 0:
+                continue
+            default_he = he_versions[0]
+            en_versions = VersionSet({"title": index.title, "actualLanguage": "en"}).array()
+            if len(en_versions) == 0:
+                continue
+            default_en = en_versions[0]
+            he_wc = default_he.word_count()
+            en_wc = default_en.word_count()
+            if en_wc/he_wc > 0.5:
+                # print(f"Skipping {index.title}. {default_en.versionTitle}")
+                continue
+            print(index.title)
             if index.get_primary_corpus():
-                counts[index.get_primary_corpus()] += 1
+                counts[index.get_primary_corpus()] += [index]
             elif len(getattr(index, 'authors', [])) == 1:
-                counts[index.authors[0]] += 1
+                counts[index.authors[0]] += [index]
             else:
-                counts[index.title] += 1
-    for k, v in sorted(counts.items(), key=lambda x: x[1], reverse=True):
-        print(k, v)
+                counts[index.title] += [index]
+    for k, v in sorted(counts.items(), key=lambda x: len(x[1]), reverse=True):
+        print("-----")
+        print(k)
+        for vv in v:
+            print('\t', vv)
+
 
 """
 quick takeaways:
