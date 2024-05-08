6c01c1bf77dfa5276f061b7e9217aa57568984ee
nsantacruz <noahssantacruz@gmail.com>
2024-05-08 14:11:37 +0300

    feat: add function to get all topics to curate

--- experiments/topic_source_curation_v2/curator.py
+++ experiments/topic_source_curation_v2/curator.py
@@ -8,9 +8,25 @@ from sefaria.helper.llm.topic_prompt import _make_llm_topic
 from sefaria_llm_interface.common.topic import Topic
 from sefaria_llm_interface.topic_prompt import TopicPromptSource
 from util.pipeline import Artifact
+import csv
 import django
 django.setup()
 from sefaria.model.topic import Topic as SefariaTopic
+
+TOPICS_TO_CURATE_CSV_PATH = 'input/Topic project plan - 1000 topics pages product - list of all topic slugs.csv'
+
+def get_topics_to_curate():
+    topics = []
+    with open(TOPICS_TO_CURATE_CSV_PATH, "r") as fin:
+        cin = csv.DictReader(fin)
+        for row in cin:
+            slug = row['slug'].strip()
+            try:
+                topics += [_make_llm_topic(SefariaTopic.init(slug))]
+            except:
+                print("Slug doesn't exist", row['slug'])
+                continue
+    return topics
 
 def curate_topic(topic: Topic) -> list[TopicPromptSource]:
     return (Artifact(topic)
