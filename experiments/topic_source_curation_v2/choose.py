b2b4e07e6bea830577e676cb1883811e8d30d598
nsantacruz <noahssantacruz@gmail.com>
2024-05-09 14:21:01 +0300

    feat: use reranker to choose best source

--- experiments/topic_source_curation_v2/choose.py
+++ experiments/topic_source_curation_v2/choose.py
@@ -6,7 +6,8 @@ Given clusters tries to
     - Fundamental sources: funadmental sources from Tanakh and Talmud etc. should be chosen. These should be few, made 2-3
     - Interesting sources: the rest of the sources should represent interesting ideas for a newcomer to Sefaria
 """
-from experiments.topic_source_curation_v2.cluster import Cluster, embed_text
+import voyageai
+from experiments.topic_source_curation_v2.cluster import Cluster, embed_text, get_text_from_source
 from sefaria_llm_interface.topic_prompt import TopicPromptSource
 from sklearn.metrics import pairwise_distances
 from util.pipeline import Artifact
@@ -14,16 +15,17 @@ import numpy as np
 
 
 def choose_ideal_sources_for_clusters(clusters: list[Cluster]) -> list[TopicPromptSource]:
-    return Artifact(clusters).pipe(choose_ideal_clusters, 20).pipe(choose_ideal_sources_for_clusters).data
+    return Artifact(clusters).pipe(choose_ideal_clusters, 20).pipe(choose_ideal_sources).data
 
 
 def choose_ideal_clusters(clusters: list[Cluster], max_clusters: int) -> list[Cluster]:
-    sorted_clusters = _sort_by_highest_avg_pairwise_distance(clusters)
     """
     Sorted in descending order from outliers to central
     Choose a few central clusters and a few outliers
     Also might want to use custom GPT sort to find "best" clusters based on various criteria
     """
+    # sorted_clusters = _sort_by_highest_avg_pairwise_distance(clusters)
+    return clusters
 
 def choose_ideal_sources(source_clusters: list[Cluster]) -> list[TopicPromptSource]:
     """
@@ -33,7 +35,19 @@ def choose_ideal_sources(source_clusters: list[Cluster]) -> list[TopicPromptSour
         Highest average pairwise cosine distance. Higher means more unique
         Fulfills category quota. Want to choose sources from different categories
     """
-    pass
+    ideal_sources = []
+    for cluster in source_clusters:
+       ideal_sources += [choose_ideal_source_from_cluster(cluster)]
+    return ideal_sources
+
+
+def choose_ideal_source_from_cluster(cluster: Cluster) -> TopicPromptSource:
+    vo = voyageai.Client()
+    output = vo.rerank(cluster.summary, [get_text_from_source(item) for item in cluster.items], "rerank-lite-1")
+    best_idx = output.results[0].index
+    return cluster.items[best_idx]
+
+
 
 
 def _get_highest_avg_pairwise_distance_indices(embeddings: np.ndarray) -> np.ndarray:
