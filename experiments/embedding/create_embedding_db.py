4fd2a1f5b77857509e37e2eb702efe8ab21c9ea8
nsantacruz <noahssantacruz@gmail.com>
2024-05-09 11:42:46 +0300

    fix: add metadata to passages

--- experiments/embedding/create_embedding_db.py
+++ experiments/embedding/create_embedding_db.py
@@ -5,6 +5,7 @@ from time import sleep
 from langchain_text_splitters import CharacterTextSplitter
 from srsly import read_jsonl
 from tqdm import tqdm
+import re
 from concurrent.futures import ThreadPoolExecutor
 from voyageai.error import RateLimitError
 import os
@@ -48,9 +49,48 @@ def ingest_document(doc, db, pbar):
 def make_doc_from_export_item(export_item):
     return Document(page_content=export_item['text'], metadata=export_item['metadata'])
 
-def ingest_all(start=0):
+def make_metadata_from_ref(oref: Ref, version: Version, pr: float):
+    return {
+        "url": f"https://www.sefaria.org/{oref.url()}",
+        "ref": oref.normal(), "versionTitle": version.versionTitle, "lang": version.actualLanguage,
+        "docCategory": version.get_index().get_primary_category(),
+        'dataQuality': 'user' if version.versionTitle == "Sefaria Community Translation" else 'professional',
+        'pagerank': pr,
+    }
+
+def ingest_passages():
+    ref_pr_map = {ref_data.ref: ref_data.pagesheetrank for ref_data in RefDataSet()}
+    docs = []
+    passage_set = PassageSet()
+    for passage in tqdm(passage_set, desc='Creating passages', total=len(passage_set)):
+        oref = Ref(passage.full_ref)
+        chunk = oref.text('en')
+        passage_text = re.sub('<[^>]+>', '', TextChunk.strip_itags(chunk.as_string()))
+        try:
+            version = chunk.version()
+        except Exception as e:
+            for sub_ref in oref.range_list():
+                try:
+                    version = sub_ref.text('en').version()
+                except:
+                    continue
+        try:
+            pr = sum(ref_pr_map[sub_ref.normal()] for sub_ref in oref.range_list())/len(oref.range_list())
+        except Exception as e:
+            print(e)
+            continue
+        metadata = make_metadata_from_ref(oref, version, pr)
+        doc = Document(page_content=passage_text, metadata=metadata)
+        docs.append(doc)
+    ingest_docs(docs)
+
+def ingest_export(start):
     export = read_jsonl(export_filename)
     docs = [make_doc_from_export_item(item) for item in export]
+    ingest_docs(docs, start)
+
+
+def ingest_docs(docs, start=0):
     embeddings = VoyageAIEmbeddings(model="voyage-large-2-instruct", batch_size=32)
     chroma_db = Chroma(persist_directory="embedding/.chromadb", embedding_function=embeddings)
     # chroma_db = Chroma.from_documents(
@@ -71,12 +111,13 @@ def query():
     query = "Abraham our forefather"
     embeddings = VoyageAIEmbeddings(model="voyage-large-2-instruct", batch_size=32)
     db = Chroma(persist_directory="embedding/.chromadb", embedding_function=embeddings)
-    ret = db.similarity_search_with_relevance_scores(query)
-    for yo in ret:
-        print(yo)
-    print("DB size", len(db.get()["ids"]))
+    # ret = db.similarity_search_with_relevance_scores(query)
+    # for yo in ret:
+    #     print(yo)
+    # print("DB size", len(db.get()["ids"]))
 
 if __name__ == '__main__':
-    ingest_all(855836)
+    # ingest_export(855836)
+    ingest_passages()
     # query()
     # add_herzog_tanakh_passages()
