9793995f5ac87a5cbaa5fb1126f32070e08c1166
nsantacruz <noahssantacruz@gmail.com>
2024-05-12 09:22:22 +0300

    feat: switch to openai embeddings for vector store

--- experiments/embedding/create_embedding_db.py
+++ experiments/embedding/create_embedding_db.py
@@ -1,4 +1,5 @@
 from langchain_voyageai import VoyageAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.docstore.document import Document
 from time import sleep
@@ -84,22 +85,24 @@ def ingest_passages():
         docs.append(doc)
     ingest_docs(docs)
 
-def ingest_export(start):
+def ingest_export(start=0):
     export = read_jsonl(export_filename)
     docs = [make_doc_from_export_item(item) for item in export]
     ingest_docs(docs, start)
 
 
 def ingest_docs(docs, start=0):
-    embeddings = VoyageAIEmbeddings(model="voyage-large-2-instruct", batch_size=32)
-    chroma_db = Chroma(persist_directory="embedding/.chromadb", embedding_function=embeddings)
-    # chroma_db = Chroma.from_documents(
-    #     documents=[docs[0]], embedding=embeddings, persist_directory="embedding/.chromadb"
-    # )
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    if start == 0:
+        chroma_db = Chroma.from_documents(
+            documents=[docs[0]], embedding=embeddings, persist_directory="embedding/.chromadb_openai"
+        )
+    else:
+        chroma_db = Chroma(persist_directory="embedding/.chromadb_openai", embedding_function=embeddings)
 
     with tqdm(total=len(docs)-1, desc="Ingesting documents") as pbar:
         pbar.update(start)
-        with ThreadPoolExecutor(max_workers=4) as executor:  # Adjust max_workers as needed
+        with ThreadPoolExecutor(max_workers=40) as executor:  # Adjust max_workers as needed
             futures = []
             for d in docs[start:]:
                 future = executor.submit(ingest_document, d, chroma_db, pbar)
@@ -109,15 +112,15 @@ def ingest_docs(docs, start=0):
 
 def query():
     query = "Abraham our forefather"
-    embeddings = VoyageAIEmbeddings(model="voyage-large-2-instruct", batch_size=32)
-    db = Chroma(persist_directory="embedding/.chromadb", embedding_function=embeddings)
-    # ret = db.similarity_search_with_relevance_scores(query)
-    # for yo in ret:
-    #     print(yo)
-    # print("DB size", len(db.get()["ids"]))
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    db = Chroma(persist_directory="embedding/.chromadb_openai", embedding_function=embeddings)
+    ret = db.similarity_search_with_relevance_scores(query)
+    for yo in ret:
+        print(yo)
+    print("DB size", len(db.get()["ids"]))
 
 if __name__ == '__main__':
-    # ingest_export(855836)
-    ingest_passages()
-    # query()
+    # ingest_export(49446)
+    # ingest_passages()
+    query()
     # add_herzog_tanakh_passages()
