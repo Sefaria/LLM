f542f6044bc5271c572e70147aef8917a345bc1e
nsantacruz <noahssantacruz@gmail.com>
2024-05-16 21:14:39 +0300

    feat: save clusters to file

--- experiments/topic_source_curation_v2/scripts/analyze_gathered_sources.py
+++ experiments/topic_source_curation_v2/scripts/analyze_gathered_sources.py
@@ -6,6 +6,7 @@ django.setup()
 from sefaria.model.topic import Topic as SefariaTopic
 from sefaria.helper.llm.topic_prompt import _make_llm_topic
 from collections import defaultdict
+import csv
 
 
 def count_cats():
@@ -29,6 +30,30 @@ def print_clusters():
             print('\t\t', item.summary)
 
 
+def save_clusters_to_csv(slug):
+    clusters = load_clusters(_make_llm_topic(SefariaTopic.init(slug)))
+    rows = []
+    for cluster in clusters:
+        for item in cluster.items:
+            rows += [{
+                "Cluster Label": cluster.label,
+                "Cluster Summary": cluster.summary,
+                "Source Summary": item.summary,
+                "Ref": item.source.ref,
+                "Text": item.source.text['en'],
+            }]
+    with open("output/clusters_{}.csv".format(slug), 'w') as fout:
+        cout = csv.DictWriter(fout, ['Cluster Label', 'Cluster Summary', 'Source Summary', 'Ref', 'Text'])
+        cout.writeheader()
+        cout.writerows(rows)
+
+
+
 if __name__ == '__main__':
     # count_cats()
-    print_clusters()
+    # print_clusters()
+    # slugs = ['ants', 'ulla', 'achitofel', 'friendship', 'david-and-the-temple', 'cains-sacrifice', 'abraham-in-egypt']
+    slugs = ['war-with-midian', 'medicine']
+    for slug in slugs:
+        print(slug)
+        save_clusters_to_csv(slug)
