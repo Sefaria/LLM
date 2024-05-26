903d465b3d81cfa313628af34ce1de35326c3d61
nsantacruz <noahssantacruz@gmail.com>
2024-05-26 12:08:43 +0300

    feat: function to create export of just hebrew

--- experiments/embedding/get_stuff_to_ingest.py
+++ experiments/embedding/get_stuff_to_ingest.py
@@ -1,5 +1,7 @@
-from srsly import read_jsonl
-export_filename = "/Users/nss/Downloads/en_library.jsonl"
+from srsly import read_jsonl, write_jsonl
+en_export_filename = "/Users/nss/Downloads/en_library.jsonl"
+he_export_filename = "/Users/nss/Downloads/he_library.jsonl"
+he_only_export_filename = "/Users/nss/Downloads/he_only_library.jsonl"
 import django
 django.setup()
 from sefaria.model import *
@@ -10,7 +12,7 @@ def make_doc_from_export_item(export_item):
     return Document(page_content=export_item['text'], metadata=export_item['metadata'])
 
 def get_passages_to_ingest():
-    export = read_jsonl(export_filename)
+    export = read_jsonl(en_export_filename)
     tref_to_export_item = {}
     for item in export:
         meta = item["metadata"]
@@ -38,7 +40,41 @@ def get_passages_to_ingest():
             })]
     return docs
 
+
+def get_hebrew_that_has_no_english_to_ingest():
+    en_export = read_jsonl(en_export_filename)
+    he_export = read_jsonl(he_export_filename)
+    he_only_export = []
+    en_refs = set()
+    for item in en_export:
+        en_refs.add(item['metadata']['ref'])
+    for item in he_export:
+        if item['metadata']['ref'] in en_refs:
+            continue
+        he_only_export += [item]
+    write_jsonl(he_only_export_filename, he_only_export)
+
+def count_tokens_in_export(export):
+    from util.openai_utils import count_tokens_openai
+    count = 0
+    for item in tqdm(list(export)):
+        count += count_tokens_openai(item['text'])
+    print('Num Tokens', count)
+
+
+def print_random_segs():
+    import random
+    export = list(read_jsonl(he_only_export_filename))
+    export = export[:len(export)//4]
+    sample = random.sample(export, 1000)
+    for item in sample:
+        print(item['metadata']['ref'])
+
+
 if __name__ == '__main__':
-    docs = get_passages_to_ingest()
-    print(docs[0])
-    print(len(docs))
+    # docs = get_passages_to_ingest()
+    # print(docs[0])
+    # print(len(docs))
+    # get_hebrew_that_has_no_english_to_ingest()
+    # count_tokens_in_export(read_jsonl(he_only_export_filename))
+    print_random_segs()
