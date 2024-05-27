4a7890e19793145021db8520544bbcc25a0d3cd8
nsantacruz <noahssantacruz@gmail.com>
2024-05-27 12:01:20 +0300

    feat: add function to try to derive useful context for a source

--- app/topic_prompt/uniqueness_of_source.py
+++ app/topic_prompt/uniqueness_of_source.py
@@ -5,8 +5,8 @@ import json
 import re
 from functools import reduce
 from typing import List
-from util.general import get_source_text_with_fallback
-
+from util.general import get_source_text_with_fallback, get_by_xml_tag
+from util.topic import get_or_generate_topic_description
 from sefaria_llm_interface.topic_prompt import TopicPromptSource
 from sefaria_llm_interface import Topic
 
@@ -17,7 +17,7 @@ from basic_langchain.chat_models import ChatOpenAI
 
 def _get_prompt_inputs(source, other_sources: List[TopicPromptSource], topic: Topic):
     topic_title = topic.title['en']
-    topic_description = topic.description.get("en", "N/A")
+    topic_description = get_or_generate_topic_description(topic, verbose=False)
     comparison_sources_list = []
     max_len = 7000
     for other_source in other_sources:
@@ -36,6 +36,36 @@ def _get_prompt_inputs(source, other_sources: List[TopicPromptSource], topic: To
 
 def get_uniqueness_of_source(source: TopicPromptSource, topic: Topic, other_sources: List[TopicPromptSource]) -> str:
     return _get_uniqueness_of_source_as_compared_to_other_sources(source, other_sources, topic)
+
+
+def get_context_of_source(source: TopicPromptSource, topic: Topic, other_sources: List[TopicPromptSource]) -> str:
+    prompt_inputs = _get_prompt_inputs(source, other_sources, topic)
+    del prompt_inputs['hint']
+    system_message = SystemMessage(content=
+                                   f"You are an expert in Jewish laws, history and traditions, wishing to teach your students a text related to {topic.title['en']}.\n"
+                                   "# Task\n"
+                                   f"Given a list of Jewish texts about '{topic.title['en']}', output helpful context for your students that doesn't exist in inputSource. This context should be gleaned from comparisonSources.\n"
+                                   "# Input format\n"
+                                   "Input will be in JSON format with the following structure\n"
+                                   '{'
+                                   '"topicTitle": "Title of the topic the sources are focusing on",'
+                                   '"topicDescription": "Description of the topic",'
+                                   '"inputSource": "Text of the source we want context for",'
+                                   '"comparisonSources": "List of text of sources to compare `inputSource` to"'
+                                   '}\n'
+                                   "# Output format\n"
+                                   "Output context for `inputSource`, extracted from `comparisonSources`, that will "
+                                   "help your students understand the source. Output should be wrapped in <context> tags. Limit context to 50 words or less."
+                                   )
+    prompt = PromptTemplate.from_template('{{{{'
+                                          '"topicTitle": "{topic_title}", "topicDescription": "{topic_description}",'
+                                          '"inputSource": "{input_source}", "comparisonSources": {comparison_sources}'
+                                          '}}}}')
+    human_message = HumanMessage(content=prompt.format(**prompt_inputs))
+    llm = ChatOpenAI(model="gpt-4o", temperature=0)
+
+    response = llm([system_message, human_message])
+    return get_by_xml_tag(response.content, 'context')
 
 
 def summarize_based_on_uniqueness(text: str, uniqueness: str, llm, lang) -> str:
@@ -91,7 +121,7 @@ def _get_uniqueness_of_source_as_compared_to_other_sources(source: TopicPromptSo
                                           '"hint": "{hint}"'
                                           '}}}}')
     human_message = HumanMessage(content=prompt.format(**prompt_inputs))
-    llm = ChatOpenAI(model="gpt-4", temperature=0)
+    llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
     response = llm([system_message, human_message])
     uniqueness = re.sub(fr'^"?{uniqueness_preamble}\s*', '', response.content)
