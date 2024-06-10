4b891910a8dc5c51d29b0d089f7189499a33eefc
nsantacruz <noahssantacruz@gmail.com>
2024-06-10 09:09:33 +0300

    chore: move to data/private

--- experiments/topic_source_curation/scripts/make_ref_topic_links.py
+++ experiments/topic_source_curation/scripts/make_ref_topic_links.py
@@ -4,7 +4,8 @@ from experiments.topic_source_curation.curator import get_topics_to_curate
 import django
 
 from sefaria.model import RefTopicLink
-
+from sefaria.model import Topic as SefariaTopic
+from sefaria.helper.llm.topic_prompt import make_llm_topic
 django.setup()
 random.seed(45612)
 
@@ -34,7 +35,7 @@ def _make_ref_topic_link(topic, tref, context, i):
 
 def save_ref_topic_links():
     import json
-    with open("scripts/ref_topic_links.json", "r") as fin:
+    with open("data/private/ref_topic_links.json", "r") as fin:
         links = json.load(fin)
     len(links)
     for link in links:
@@ -43,6 +44,19 @@ def save_ref_topic_links():
 def _generate_all_prompts():
     from tqdm import tqdm
     slugs_to_generate = {l.toTopic for l in RefTopicLinkSet({"generatedBy": "auto-curator"})}
+    slugs_to_generate = [
+        'balaam',
+        'caleb',
+        'parents',
+        'parah-adumah',
+        'hunger',
+        'disability',
+        'aarons-death',
+        'josephs-dream',
+        'empathy',
+        'leviathan',
+        'memory',
+    ]
     for slug in tqdm(slugs_to_generate):
         _generate_prompts_for_slug(slug)
 
@@ -58,7 +72,10 @@ def _generate_prompts_for_slug(slug):
 
 if __name__ == '__main__':
     links = []
-    topics = random.sample(get_topics_to_curate(), 50)
+    # topics = random.sample(get_topics_to_curate(), 50)
+    topics = [make_llm_topic(SefariaTopic.init(slug)) for slug in [
+        'creation-of-man'
+    ]]
     for topic in topics:
         print(topic.slug)
         with open(f"output/curation_{topic.slug}.json", "r") as fin:
