6856d85e2bf6d42605afbb5fee0baf7b167f7774
nsantacruz <noahssantacruz@gmail.com>
2024-07-16 09:08:07 +0300

    feat: add ability to set start end indices to make_ref_topic_links.py

--- experiments/topic_source_curation/scripts/make_ref_topic_links.py
+++ experiments/topic_source_curation/scripts/make_ref_topic_links.py
@@ -33,6 +33,117 @@ def _make_ref_topic_link(topic, tref, context, i):
     }
 
 
+def find_topics_that_didnt_generate():
+    from tqdm import tqdm
+    slugs_to_generate = [
+        "lilies",
+        "arbeh",
+        "man-and-animals",
+        "mice",
+        "mountains",
+        "mules",
+        "myrtles",
+        "night",
+        "oxen",
+        "pigs",
+        "rams",
+        "ravens",
+        "rivers",
+        "roosters",
+        "science",
+        "scorpions",
+        "seas1",
+        "sherets",
+        "snakes1",
+        "snow",
+        "spring",
+        "springs",
+        "sun",
+        "sustainability",
+        "the-elements",
+        "the-moon",
+        "the-ocean",
+        "vines",
+        "water",
+        "weather",
+        "wild-animals",
+        "willows",
+        "winds",
+        "wolves",
+        "worms",
+        "yavneh",
+        "edom",
+        "the-four-kingdoms",
+        "alexandria",
+        "the-four-rivers",
+        "distinctions-of-the-land-of-israel",
+        "babel",
+        "beersheba",
+        "beit-el",
+        "beit-hamikdash",
+        "galil",
+        "germany",
+        "damascus",
+        "har-gerizim-vehar-eival",
+        "moriah",
+        "outside-of-israel",
+        "the-land-of-judah",
+        "greece",
+        "jerusalem",
+        "kush",
+        "levite-cities",
+        "midian",
+        "moab",
+        "partitions",
+        "encampment-of-israel",
+        "cave-of-the-patriarchs",
+        "tabernacle-courtyard",
+        "the-nile-river",
+        "nineveh",
+        "sodom",
+        "syria",
+        "ever-hayarden",
+        "cities",
+        "ammon",
+        "gemorrah",
+        "tyre",
+        "zion",
+        "rome",
+        "the-tabernacle-in-shiloh",
+        "shechem",
+        "heavens",
+        "sharon",
+        "tarshish",
+        "beit-midrash",
+        "temple-mount",
+        "second-temple",
+        "en-dor",
+        "jericho",
+        "israel",
+        "ashur",
+        "food",
+        "honey",
+        "olives",
+        "milk",
+        "wine",
+        "fresh-grain",
+        "maror",
+        "planting",
+        "peppers",
+        "fruit",
+        "flour",
+        "drinking",
+    ]
+
+    bad_slugs = []
+    for slug in tqdm(slugs_to_generate):
+        topic_links = RefTopicLinkSet({"descriptions.en.ai_title": {"$exists": True}, "toTopic": slug})
+        if len(topic_links) == 0:
+            bad_slugs.append(slug)
+    for slug in tqdm(bad_slugs):
+        print(slug)
+        _generate_prompts_for_slug(slug)
+
 def save_ref_topic_links():
     import json
     from tqdm import tqdm
@@ -57,7 +168,103 @@ def save_ref_topic_links():
 def _generate_all_prompts():
     from tqdm import tqdm
     slugs_to_generate = [
-        'rebellions'
+        "lilies",
+        "arbeh",
+        "man-and-animals",
+        "mice",
+        "mountains",
+        "mules",
+        "myrtles",
+        "night",
+        "oxen",
+        "pigs",
+        "rams",
+        "ravens",
+        "rivers",
+        "roosters",
+        "science",
+        "scorpions",
+        "seas1",
+        "sherets",
+        "snakes1",
+        "snow",
+        "spring",
+        "springs",
+        "sun",
+        "sustainability",
+        "the-elements",
+        "the-moon",
+        "the-ocean",
+        "vines",
+        "water",
+        "weather",
+        "wild-animals",
+        "willows",
+        "winds",
+        "wolves",
+        "worms",
+        "yavneh",
+        "edom",
+        "the-four-kingdoms",
+        "alexandria",
+        "the-four-rivers",
+        "distinctions-of-the-land-of-israel",
+        "babel",
+        "beersheba",
+        "beit-el",
+        "beit-hamikdash",
+        "galil",
+        "germany",
+        "damascus",
+        "har-gerizim-vehar-eival",
+        "moriah",
+        "outside-of-israel",
+        "the-land-of-judah",
+        "greece",
+        "jerusalem",
+        "kush",
+        "levite-cities",
+        "midian",
+        "moab",
+        "partitions",
+        "encampment-of-israel",
+        "cave-of-the-patriarchs",
+        "tabernacle-courtyard",
+        "the-nile-river",
+        "nineveh",
+        "sodom",
+        "syria",
+        "ever-hayarden",
+        "cities",
+        "ammon",
+        "gemorrah",
+        "tyre",
+        "zion",
+        "rome",
+        "the-tabernacle-in-shiloh",
+        "shechem",
+        "heavens",
+        "sharon",
+        "tarshish",
+        "beit-midrash",
+        "temple-mount",
+        "second-temple",
+        "en-dor",
+        "jericho",
+        "israel",
+        "ashur",
+        "food",
+        "honey",
+        "olives",
+        "milk",
+        "wine",
+        "fresh-grain",
+        "maror",
+        "planting",
+        "peppers",
+        "fruit",
+        "flour",
+        "drinking",
     ]
 
     for slug in tqdm(slugs_to_generate):
@@ -76,15 +283,17 @@ def _generate_prompts_for_slug(slug):
 if __name__ == '__main__':
     links = []
     print(len(get_topics_to_curate()))
-    # topics = get_topics_to_curate()[:150]
-    topics = [make_llm_topic(SefariaTopic.init(slug)) for slug in [
-        'naval'
-    ]]
-    for topic in topics:
+    all_topics = get_topics_to_curate()
+    istart = next((i for i, topic in enumerate(all_topics) if topic.slug == "lilies"), None)
+    iend = next((i for i, topic in enumerate(all_topics) if topic.slug == "drinking"), None)
+    for topic in all_topics[istart:iend+1]:
         print(f'"{topic.slug}",')
-        with open(f"output/curation_{topic.slug}.json", "r") as fin:
-            curation = json.load(fin)
-            for i, entry in enumerate(curation):
-                links += [_make_ref_topic_link(topic, entry['ref'], entry['context'], len(curation)-i)]
+        try:
+            with open(f"output/curation_{topic.slug}.json", "r") as fin:
+                curation = json.load(fin)
+                for i, entry in enumerate(curation):
+                    links += [_make_ref_topic_link(topic, entry['ref'], entry['context'], len(curation)-i)]
+        except:
+            print("SKIP", topic.slug)
     with open(f"output/ref_topic_links.json", "w") as fout:
         json.dump(links, fout, ensure_ascii=False, indent=2)
