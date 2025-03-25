c8c147ee4713693d42882acb7ccf369b936bc454
nsantacruz <noahssantacruz@gmail.com>
2025-03-25 10:45:01 +0200

    fix: improve scoring calculation

--- experiments/linker/fine_tune/project_scripts/diff_prodigy_collections.py
+++ experiments/linker/fine_tune/project_scripts/diff_prodigy_collections.py
@@ -20,10 +20,10 @@ def hash_span(span):
     return hash((span['start'], span['end'], span['label']))
 
 
-def get_num_diff_spans(orig_doc, mod_doc):
+def get_num_spans_in_common(orig_doc, mod_doc):
     hashed_orig = {hash_span(span) for span in orig_doc['spans']}
     hashed_mod = {hash_span(span) for span in mod_doc['spans']}
-    return len(hashed_mod - hashed_orig)
+    return len(hashed_mod & hashed_orig)
 
 
 def init_argparse() -> argparse.ArgumentParser:
@@ -34,17 +34,25 @@ def init_argparse() -> argparse.ArgumentParser:
     return parser
 
 
-if __name__ == '__main__':
-    parser = init_argparse()
-    args = parser.parse_args()
+def convert_en_labels_to_he(doc):
+    for span in doc['spans']:
+        if span['label'] == 'Citation':
+            span['label'] = 'מקור'
+        elif span['label'] == 'Person':
+            span['label'] = 'בן-אדם'
+        else:
+            raise Exception("Unknown label: {}".format(span['label']))
+    return doc
 
-    original_docs = list(load_mongo_docs(args.original_collection))
-    modified_docs = list(load_mongo_docs(args.modified_collection))
+
+def diff_prodigy_collections(original_collection, modified_collection, output_collection):
+    original_docs = list(load_mongo_docs(original_collection))
+    modified_docs = list(load_mongo_docs(modified_collection))
 
     hashed_original = {hash_doc(d): d for d in original_docs}
     hashed_original_wo_spans = {hash_doc(d, include_spans=False): d for d in original_docs}
     diff_docs = []
-    num_diff_spans = 0
+    num_common_spans = 0
     total_spans = reduce(lambda a, b: a + len(b['spans']), original_docs, 0)
     for modified_doc in modified_docs:
         modified_hash = hash_doc(modified_doc)
@@ -52,12 +60,20 @@ if __name__ == '__main__':
             diff_docs += [modified_doc]
             modified_hash_wo_spans = hash_doc(modified_doc, include_spans=False)
             try:
-                num_diff_spans += get_num_diff_spans(hashed_original_wo_spans[modified_hash_wo_spans], modified_doc)
+                num_common_spans += get_num_spans_in_common(hashed_original_wo_spans[modified_hash_wo_spans], modified_doc)
             except KeyError:
                 print('key error')
+        else:
+            # docs are the same
+            num_common_spans += get_num_spans_in_common(modified_doc, modified_doc)
 
-    my_db = MongoProdigyDBManager(args.output_collection)
+    my_db = MongoProdigyDBManager(output_collection)
     my_db.output_collection.delete_many({})
-    my_db.output_collection.bulk_write([InsertOne(d) for d in diff_docs])
-    print("Accuracy:", (total_spans-num_diff_spans)/total_spans)
+    my_db.output_collection.bulk_write([InsertOne(convert_en_labels_to_he(d)) for d in diff_docs])
+    print("Accuracy:", num_common_spans/total_spans)
 
+
+if __name__ == '__main__':
+    parser = init_argparse()
+    args = parser.parse_args()
+    diff_prodigy_collections(args.original_collection, args.modified_collection, args.output_collection)
