27bd4b1c0f704eb8bafff11b54d7c00c914eb597
nsantacruz <noahssantacruz@gmail.com>
2025-03-25 10:44:02 +0200

    feat: improve sentencize script so it's easily reusable

--- experiments/linker/fine_tune/one_time_scripts/sentencize_training_data.py
+++ experiments/linker/fine_tune/one_time_scripts/sentencize_training_data.py
@@ -1,18 +1,37 @@
 from db_manager import MongoProdigyDBManager
 from util.sefaria_specific import load_mongo_docs
-from util.sentencizer import sentencize
+from util.sentencizer import sentencize, claude_sentencizer
+from util.general import run_parallel
+from functools import partial
 from tqdm import tqdm
 
+import logging
 
-def segmentize_and_save(my_db, text, meta):
-    sents = sentencize(text)
-    for sent in sents:
-        my_db.output_collection.insert_one({"text": sent, "spans": [], "meta": meta})
+# Set the logging level for 'httpx' to WARNING
+httpx_logger = logging.getLogger('httpx')
+httpx_logger.setLevel(logging.WARNING)
+
+
+def sentencize_by_type(text, sentencizer_type='claude'):
+    if sentencizer_type == 'claude':
+        sents = claude_sentencizer(text)
+    else:
+        sents = sentencize(text)
+    return sents
+
+
+def sentencize_training_data(input_collection, output_collection, skip, total, sentencizer_type):
+    my_db = MongoProdigyDBManager(output_collection)
+    my_db.output_collection.delete_many({})
+    docs = load_mongo_docs(input_collection)
+    docs= list(docs)[skip:skip+total]
+    unit_func = partial(sentencize_by_type, sentencizer_type=sentencizer_type)
+    all_sents = run_parallel([d['text'] for d in docs], unit_func, 25, desc='Sentencizing')
+    for doc, sents in zip(docs, all_sents):
+        for sent in sents:
+            if len(sent) < 20: continue
+            my_db.output_collection.insert_one({"text": sent, "spans": [], "meta": doc['meta']})
 
 
 if __name__ == '__main__':
-    my_db = MongoProdigyDBManager('ner_en_sent_input2')
-    my_db.output_collection.delete_many({})
-    docs = load_mongo_docs('ner_en_web_input')
-    for doc in tqdm(list(docs)):
-        segmentize_and_save(my_db, doc['text'], doc['meta'])
+    sentencize_training_data('ner_he_input', 'ner_he_input_broken', 21500, 500, 'claude')
